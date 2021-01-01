acf30ca7ef63461aae4409e4fa461f3444c128bc
Marko Pavlenko <marko.pavlenko@nure.ua>
2021-01-01 16:46:38 +0200

    Implemented parsing Var from URL`s

--- index.js
+++ index.js
@@ -61,14 +61,14 @@ const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
-    console.log(req.url);
-    console.log(url.parse(req.url, true));
-    const pathName = req.url;
+
+    const { query, pathname } = url.parse(req.url, true);
+    //const pathname = req.url;
 
     //Overview page
 
     //hand-made routing
-    if (pathName === '/' || pathName === '/overview') {
+    if (pathname === '/' || pathname === '/overview') {
         res.writeHead(200, { 'Content-type': 'text/html' });
 
         const cardsHTML = dataObj.map(el => replaceTemplate(tempCard, el)).join();
@@ -77,11 +77,14 @@ const server = http.createServer((req, res) => {
         res.end(output);
 
         //Product page
-    } else if (pathName === '/product') {
-        res.end('This is a product page!');
+    } else if (pathname === '/product') {
+        res.writeHead(200, { 'Content-type': 'text/html' });
+        const product = dataObj[query.id];
+        const output = replaceTemplate(tempProduct, product);
+        res.end(output);
 
-        //API
-    } else if (pathName === '/api') {
+        //Api
+    } else if (pathname === '/api') {
         res.writeHead(200, { 'Content-type': 'application/json' });
         res.end(data);
 
